64d1c3ea6bec7642c96212b5a8ee4b596cc54798
Diana Brik <di.brik@yandex.kz>
2022-12-21 11:59:41 +0600

    Seminar 18.12.22 Task 10

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99) && (number < 1000))
+{
+    number = number % 100 / 10;
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
